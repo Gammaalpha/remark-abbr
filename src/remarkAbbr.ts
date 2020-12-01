30a7aa6fa094818ec44c18edf2a79ed59fe08c4d
Faiq Malik <faiqmalik2@gmail.com>
2020-11-30 21:55:26 -0500

    working abbr plugin

--- src/remarkAbbr.ts
+++ src/remarkAbbr.ts
@@ -1,8 +1,5 @@
 let visit = require('unist-util-visit');
-// const u = require('unist-builder');
-let remove = require('unist-util-remove');
 var squeezeParagraphs = require('mdast-squeeze-paragraphs')
-
 
 export interface AbbrProps {
     text: string,
@@ -14,25 +11,28 @@ export const RemarkAbbr = () => {
     return transformer
 
     function transformer(tree: any, file: any) {
+
         const keepNodes: AbbrProps[] = [];
         const regex = new RegExp(/[*]\[([^\]]*)\]:\s*(.+)\n*/)
         // get abbr data values
         visit(tree, "paragraph",
             (node: any) => {
+                if (node.type !== 'paragraph') {
+                    return node;
+                }
                 if (node.type === "paragraph") {
                     let lastChild = node.children[node.children.length - 1];
 
                     if (lastChild && lastChild.type === "text") {
                         const nodeCheck = regex.exec(lastChild.value);
-                        // console.log("Keeping: ", nodeCheck);
                         if (nodeCheck !== null) {
                             keepNodes.push({
                                 text: nodeCheck[1],
                                 title: nodeCheck[2]
                             });
+                            //clear out the abbr input data
                             node.children[node.children.length - 1].value = "";
                         }
-                        //clear out the abbr input data
                     }
                 }
             })
@@ -41,13 +41,28 @@ export const RemarkAbbr = () => {
 
         // get abbr text values to replace
         visit(tree, "text", (node: any, index: number, parent: any) => {
-            console.log("Nodes: ", node);
+            const position = node.position;
+
             keepNodes.forEach(abbr => {
                 if (abbr.text === node.value) {
-                    node.value = `<abbr title=${abbr.title}>${abbr.text}</abbr>`
+                    const newChild = {
+                        type: 'element',
+                        children: [
+                            { type: "text", value: abbr.text },
+                        ],
+                        data: {
+                            hName: 'abbr',
+                            hProperties: {
+                                title: abbr.title
+                            }
+                        },
+                        position
+                    }
+
+                    parent.children[index] = newChild;
+
                 }
             })
         })
-        // console.log("tree after: ", JSON.stringify(tree, null, 2));
     }
 }
