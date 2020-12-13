f335483d5525803999b96f846853e9bb03194fb2
Faiq Malik <faiqmalik2@gmail.com>
2020-12-13 01:04:09 -0500

    update

--- src/tests/first.spec.ts
+++ src/tests/first.spec.ts
@@ -1,17 +1,23 @@
 import Plugin from "./plugin";
 // let Plugin = require("./plugin");
 
-let result = Plugin();
-test('should have the abbr translated node as is', () => {
-    expect(result).toMatch(/(\<abbr title="hypertext"\>html\<\/abbr\>)/i);
-})
+
+let result = "";
+describe("not section", () => {
+    beforeAll(() => {
+        result = Plugin();
+    })
+    it('should have the abbr translated node as is', () => {
+        expect(result).toMatch(/(\<abbr title="hypertext"\>html\<\/abbr\>)/i);
+    })
 
 
-test('should have the abbr translated node in strong tag', () => {
-    expect(result).toMatch(/(\<strong\>\<abbr title="testing"\>test\<\/abbr\>\<\/strong\>)/i);
-})
+    it('should have the abbr translated node in strong tag', () => {
+        expect(result).toMatch(/(\<strong\>\<abbr title="testing"\>test\<\/abbr\>\<\/strong\>)/i);
+    })
 
 
-test('should have the abbr translated node in table cell tag', () => {
-    expect(result).toMatch(/(\<td align="center"\>Put Pipes In \<abbr title="hypertext"\>html\<\/abbr\>\<\/td\>)/i);
-})
+    it('should have the abbr translated node in table cell tag', () => {
+        expect(result).toMatch(/(\<td align="center"\>Put Pipes In \<abbr title="hypertext"\>html\<\/abbr\>\<\/td\>)/i);
+    })
+});
--- src/tests/second.spec.ts
+++ src/tests/second.spec.ts
@@ -1,12 +1,15 @@
 import Plugin from "./plugin";
 
-let result = Plugin();
-console.log("test res:", result);
+let result = "";
+describe("not section", () => {
+    beforeAll(() => {
+        result = Plugin();
+    })
+    it('should not have the abbr declaration node', () => {
+        expect(result).not.toContain(/(\*\[html\]\:hypertext)/i);
+    })
 
-test('should not have the abbr declaration node', () => {
-    expect(result).not.toContain(/(\*\[html\]\:hypertext)/i);
-})
-
-test('should not have the abbr nested', () => {
-    expect(result).not.toMatch(/(\<abbr title="hypertext"\>\<abbr title="hypertext"\>html\<\/abbr\>\<\/abbr\>)/i);
-})
+    it('should not have the abbr nested', () => {
+        expect(result).not.toMatch(/(\<abbr title="hypertext"\>\<abbr title="hypertext"\>html\<\/abbr\>\<\/abbr\>)/i);
+    })
+});
